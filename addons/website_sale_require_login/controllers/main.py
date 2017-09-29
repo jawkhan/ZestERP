00d42e281879d29c14faa959ee609160efa7f569
teckzilla <girish.joshi@teckzilla.net>
2017-09-29 19:10:43 +0530

    Added saas modules

---
+++ addons/website_sale_require_login/controllers/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería, S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import http
+# from openerp.addons.website_sale.controllers.main import website_sale
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class RequireLoginToCheckout(WebsiteSale):
+    @http.route(auth="user")
+    def checkout(self, **post):
+        return super(RequireLoginToCheckout, self).checkout(**post)
